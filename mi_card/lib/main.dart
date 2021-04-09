90c0162fa2bb1d972e9975f2315c4d91d9c9dc24
ssamko <teacher.ssamko@gmail.com>
2021-04-09 22:57:27 +0900

    png

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 20),
+            padding: EdgeInsets.all(20),
+            child: Text("Hello"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
