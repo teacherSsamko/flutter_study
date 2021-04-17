44c0042b4f884ffc383cb5584b8a26297894901c
ssamko <teacher.ssamko@gmail.com>
2021-04-17 22:14:03 +0900

    feat: refactor
    now GestureDetector is inside of ReusableCard.
    setState function should be inside of void function!

--- bmi-calculator-flutter/lib/reusable_card.dart
+++ bmi-calculator-flutter/lib/reusable_card.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.color, this.cardChild});
+  ReusableCard({@required this.color, this.cardChild, this.onPress});
 
   final Color color;
   final Widget cardChild;
+  final Function onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
 }
+// GestureDetector(
+// onTap: () {
+// setState(() {
+// selectedGender = Gender.male;
+// });
+// },
+// child: ReusableCard(
+// color: selectedGender == Gender.male
+// ? activeCardColor
+//     : inactiveCardColor,
+// cardChild: IconContent(
+// icon: FontAwesomeIcons.mars,
+// label: 'MALE',
+// ),
+// ),
+// ),
