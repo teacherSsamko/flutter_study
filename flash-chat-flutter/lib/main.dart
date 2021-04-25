59a5ef0451276b0869f4a9c2c1f64a4743eaaedb
ssamko <teacher.ssamko@gmail.com>
2021-04-25 10:59:09 +0900

    feat: route with static variable

--- flash-chat-flutter/lib/main.dart
+++ flash-chat-flutter/lib/main.dart
@@ -4,6 +4,13 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -15,7 +22,14 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/chat': (context) => ChatScreen(),
+        '/register': (context) => RegistrationScreen(),
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
